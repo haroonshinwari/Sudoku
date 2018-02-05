fa5f071f9e1b2fb705f6a045c3ede2a6f2607c43
Haroon Shinwari <Haroon@Haroons-MacBook-Pro.local>
2018-02-05 20:32:28 +0000

    defined convertToSets function

--- sudoku.py
+++ sudoku.py
@@ -5,3 +5,12 @@ def read_sudoku(file):
     data = stream.readlines()
     stream.close()
     return eval("".join(data))
+
+def convertToSets(problem):
+    for row in problem:
+        for index, element in enumerate(row):
+            if element == 0:
+                row[index] = {1,2,3,4,5,6,7,8,9}
+            else:
+                row[index] = {element}
+    return problem
