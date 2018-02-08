0a8bbb72af0368345fc59786c943ef3dcef43314
Haroon Shinwari <Haroon@Haroons-MacBook-Pro.local>
2018-02-08 17:00:03 +0000

    test for eliminate

--- sudokuTEST.py
+++ sudokuTEST.py
@@ -14,12 +14,19 @@ def testConvertToInts():
 
 def testGetRowLocations():
     lst = [(5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8)]
-    assert(set(lst), set(getRowLocations(5)))
+    assert lst == getRowLocations(5)))
 
 def testGetColumnLocations():
     lst = [(0, 5), (1, 5), (2, 5), (3, 5), (4, 5), (5, 5), (6, 5), (7, 5), (8, 5)]
-    assert(set(lst), set(getColumnLocations(5)))
+    assert lst == (getColumnLocations(5)))
 
 def testGetBoxLocations():
     lst = [(3, 0), (3, 1), (3, 2), (4, 0), (4, 1), (4, 2), (5, 0), (5, 1), (5, 2)]
-    assert(set(lst), set(getBoxLocations((3, 2))))
+    assert lst == getBoxLocations((3, 2))))
+
+def testEliminate(self):
+    sets = [[{1, 2}, {3}, {4}], [{1}, {3, 5, 7}, {2}], [{2, 3}, {2}, {1, 2, 3}]]
+    location = (1, 2) # contains {2}
+    count = eliminate(sets, location, [(0, 0), (1, 0), (2, 2)])
+    assert(2, count)
+    assert sets == [[{1}, {3}, {4}], [{1}, {3, 5, 7}, {2}], [{2, 3}, {2}, {1, 3}]]
